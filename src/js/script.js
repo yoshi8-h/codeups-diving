81568e3e0ba6076ef6909e5d31714beb1e97e9e5
yoshi8-h <hirao.y.freelance@gmail.com>
2024-05-06 13:38:32 +0900

    画像のスライドアニメーション実装完了時

--- src/js/script.js
+++ src/js/script.js
@@ -65,3 +65,32 @@ const campaignSwiper = new Swiper("#js-campaign-swiper", {
 });
 
 /* -------------------------------------------------------------------------------- */
+/* 背景色の後に画像やテキストが表示されるエフェクト(アニメーション) */
+
+//要素の取得とスピードの設定
+var box = $('.js-img-colorbox'),
+  speed = 700;
+
+//.js-img-colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+  $(this).append('<div class="color"></div>')
+  var color = $(this).find($('.color')),
+  image = $(this).find('img');
+  var counter = 0;
+
+  image.css('opacity','0');
+  color.css('width','0%');
+  //inviewを使って背景色が画面に現れたら処理をする
+  color.on('inview', function(){
+  if(counter == 0){
+    $(this).delay(200).animate({'width':'100%'},speed,function(){
+          image.css('opacity','1');
+          $(this).css({'left':'0' , 'right':'auto'});
+          $(this).animate({'width':'0%'},speed);
+        })
+      counter = 1;
+    }
+  });
+});
+
+/* -------------------------------------------------------------------------------- */
